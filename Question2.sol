35647f64319978988389c909041d03ed9c9144d0
Asila4ahmed <114068233+Asila4ahmed@users.noreply.github.com>
2022-10-01 21:34:54 +0100

    Create Question2.sol

---
+++ Question2.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+//2. Give 2 examples of the following: fixed array and dynamic array
+contract Group1{
+    //fixed array
+ /*example 1*/   uint [5] public bockchain = [1,2,3,4,5];
+ /*example 2*/   uint [10] public technology = [1,2,3,4,5,6,7,8,9,0];
+
+ //dynamic array
+ /*example 1*/  uint [] public women = [1,2,3,4,5,6,7,8,9,0];
+ /*exampe 2*/   uint [] public techsters = [1,2];
