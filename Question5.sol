49b9e2034874395fb1d869649e5310122a1f16c1
Asila4ahmed <114068233+Asila4ahmed@users.noreply.github.com>
2022-10-01 21:38:14 +0100

    Create Question5.sol

---
+++ Question5.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+//5. Create a function that consist of if and else statement, and give an example with it.
+ contract Group1{
+     function anu(uint _x) public pure returns(uint){
+         if(_x > 20){
+             return 2;
+         }else {
+             return 5;
+            }
+
+     }
+ }
